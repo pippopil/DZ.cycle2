ad776f6fac055be148b8e0d9408f4b029f4095d8
pippopil <voood234@gmail.com>
2022-01-06 17:05:07 +0300

    Updated test shouldAboveAverage

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -54,7 +54,7 @@ class StatsServiceTest {
         StatsService service = new StatsService();
         long[] generalSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 5;
-        int actual = service.minSales(generalSales);
+        int actual = service.AboveAverage(generalSales);
         assertEquals(expected, actual);
     }
 }
